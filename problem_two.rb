880d2d49a58b320c3f04786175cb1cb951639a19
Supermalonz <kavekhunsri@gmail.com>
2022-05-06 14:15:29 +0700

    create branch for problem 2

---
+++ problem_two.rb
@@ -0,0 +1,12 @@
+class ProblemTwo
+
+  def longestString(num_a, num_b, num_c)
+    pivot = {"a": num_a, "b": num_b, "c": num_c}
+    longest_string = ""
+
+    while True
+      max_string = 0
+      max_count = 0
+    end
+  end
+end
