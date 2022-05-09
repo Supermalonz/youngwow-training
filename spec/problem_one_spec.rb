e2fe3665a3d3c3e6aba0d75f7682b6e4a5726141
Supermalonz <kavekhunsri@gmail.com>
2022-05-09 10:50:54 +0700

    update problem 1

--- spec/problem_one_spec.rb
+++ spec/problem_one_spec.rb
@@ -1,8 +1,23 @@
-require './problem_one.rb'
+require './problem_one'
 
 RSpec.describe ProblemOne do
   it 'return largest number from integer' do
     problem_one = ProblemOne.new
     expect(problem_one.largest_number(5123)).to eq 5321
+  end
+
+  it 'return largest number from integer' do
+    problem_one = ProblemOne.new
+    expect(problem_one.largest_number(44163)).to eq 64431
+  end
+
+  it 'return largest number from integer' do
+    problem_one = ProblemOne.new
+    expect(problem_one.largest_number(213)).to eq 321
+  end
+
+  it 'return largest number from integer' do
+    problem_one = ProblemOne.new
+    expect(problem_one.largest_number(553)).to eq 553
   end
 end
