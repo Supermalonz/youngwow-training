e153a95ed7a3b24e2286a0616a6a92b0c180cf5b
Supermalonz <kavekhunsri@gmail.com>
2022-05-09 16:29:25 +0700

    update coding convention

--- spec/problem_one_spec.rb
+++ spec/problem_one_spec.rb
@@ -1,23 +1,21 @@
 require './problem_one'
 
 RSpec.describe ProblemOne do
-  it 'return largest number from integer' do
-    problem_one = ProblemOne.new
-    expect(problem_one.largest_number(5123)).to eq 5321
+  let(:instance) { ProblemOne.new }
+
+  it 'return 5321' do
+    expect(instance.largest_number(5123)).to eq 5321
   end
 
-  it 'return largest number from integer' do
-    problem_one = ProblemOne.new
-    expect(problem_one.largest_number(44163)).to eq 64431
+  it 'return 64431' do
+    expect(instance.largest_number(44_163)).to eq 64_431
   end
 
-  it 'return largest number from integer' do
-    problem_one = ProblemOne.new
-    expect(problem_one.largest_number(213)).to eq 321
+  it 'return 321' do
+    expect(instance.largest_number(213)).to eq 321
   end
 
-  it 'return largest number from integer' do
-    problem_one = ProblemOne.new
-    expect(problem_one.largest_number(553)).to eq 553
+  it 'return 553' do
+    expect(instance.largest_number(553)).to eq 553
   end
 end
